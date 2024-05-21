ad2a4a9e05e383393297235159ca92a13dee1bef
Khip01 <aakhifathallah24@gmail.com>
2024-05-21 17:52:44 +0700

    feat: add some values and theme color

--- lib/presentation/extension/theme.dart
+++ lib/presentation/extension/theme.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 
 // Color App Template
-Color custPrimaryColor = const Color.fromRGBO(78, 1, 137, 1);
+Color custPrimaryColor = const Color.fromRGBO(57, 73, 171, 1);
+Color custSecondaryColor = const Color.fromRGBO(120, 144, 205, 1);
 Color custRedErrorColor = const Color.fromRGBO(251, 52, 79, 1);
 Color custWhiteColor = const Color.fromRGBO(255, 255, 255, 1);
+Color custBlackThemeColor = const Color.fromRGBO(32, 33, 35, 1);
 Color custBlackColor = const Color.fromRGBO(0, 12, 20, 1);
 Color custGreyColor = const Color.fromRGBO(153, 158, 161, 1);
+Color custShadowColor = const Color.fromRGBO(210, 210, 210, 1.0);
