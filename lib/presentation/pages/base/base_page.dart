a34cbf6f51409cc1fee01a241ad939e7ca1722eb
Khip01 <aakhifathallah24@gmail.com>
2024-05-21 17:55:52 +0700

    refactor: dashboard -> base_page

--- lib/presentation/pages/base/base_page.dart
+++ lib/presentation/pages/base/base_page.dart
@@ -1,9 +1,14 @@
 // ignore_for_file: camel_case_types, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:sistem_kesehatan_flutter/presentation/blocs/auth/auth_bloc.dart';
+import 'package:sistem_kesehatan_flutter/presentation/extension/extension.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/base_page_view/dashboard_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/base_page_view/dokter_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/base_page_view/pasien_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/base_page_view/rekam_medis_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/base_page_view/transaksi_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/widgets/bottom_navbar.dart';
 
 class BasePage extends StatefulWidget {
   const BasePage({super.key});
@@ -16,52 +21,41 @@ class _BasePageState extends State<BasePage> {
   @override
   void initState() {
     super.initState();
-    // Navigasi ke dashboard setiap kali halaman diinisialisasi
+    // Navigasi ke base setiap kali halaman diinisialisasi
     WidgetsBinding.instance.addPostFrameCallback((_) {
       context.goNamed('base');
     });
   }
 
+  final PageController basePageController = PageController(
+    initialPage: 0,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          color: Colors.green,
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              BlocListener<AuthBloc, AuthState>(
-                listener: (context, state) {
-                  state.maybeWhen(
-                    logoutSuccess: () {
-                      context.goNamed('login');
-                    },
-                    error: (message) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                          content: Text(message),
-                          backgroundColor: Colors.red,
-                        ),
-                      );
-                    },
-                    orElse: () {},
-                  );
-                },
-                child: OutlinedButton(
-                  onPressed: () {
-                    context.read<AuthBloc>().add(const AuthEvent.logout());
-                  },
-                  child: const Text('Logout'),
-                ),
-              ),
+      body: Column(
+        children: [
+          Flexible(
+            fit: FlexFit.tight,
+            child: PageView(
+              controller: basePageController,
+              children: [
+                DashboardPage(),
+                DokterPage(),
+                PasienPage(),
+                RekamMedisPage(),
+                TransaksiPage(),
+              ],
+            ),
+          ),
+          BottomNavbar(
+            backgroundColor: custWhiteColor,
+            itemsBottomNavbar: [
+
             ],
           ),
-        ),
+        ],
       ),
     );
   }
