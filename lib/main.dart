eb903be1ea40f6b41bbad2653d11a91cf2f405bf
Khip01 <aakhifathallah24@gmail.com>
2024-05-21 17:58:04 +0700

    fix: theming and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sistem_kesehatan_flutter/data/datasource/local_datasources/auth_local_datasources.dart';
 import 'package:sistem_kesehatan_flutter/data/datasource/remote_datasources/auth_remote_datasource.dart';
 import 'package:sistem_kesehatan_flutter/presentation/blocs/auth/auth_bloc.dart';
+import 'package:sistem_kesehatan_flutter/presentation/extension/theme.dart';
 import 'package:sistem_kesehatan_flutter/presentation/pages/base/base_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/setting/setting_page.dart';
+import 'package:sistem_kesehatan_flutter/presentation/pages/setting/user_info_page.dart';
 
 import 'presentation/widgets/default_transition_page.dart';
 import 'presentation/pages/auth/login_page.dart';
@@ -13,6 +17,12 @@ import 'presentation/pages/auth/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
+  // Mengubah warna background tombol navigasi di bagian bawah di android
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    systemNavigationBarColor: custWhiteColor,
+    statusBarBrightness: Brightness.light, // Untuk iOS, atur menjadi light agar status bar terang
+  ));
 }
 
 String initialRoute = '/login';
@@ -22,8 +32,10 @@ final _router = GoRouter(
       name: 'login',
       path: '/login',
       pageBuilder: (BuildContext context, state) {
-        return buildPageWithDefaultTransition(context: context, state: state, child:
-          FutureBuilder<bool>(
+        return buildPageWithDefaultTransition(
+          context: context,
+          state: state,
+          child: FutureBuilder<bool>(
             future: AuthLocalDataSource().isUserLoggedIn(),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
@@ -47,13 +59,35 @@ final _router = GoRouter(
       name: 'signup',
       path: '/signup',
       pageBuilder: (BuildContext context, state) {
-        return buildPageWithDefaultTransition(context: context, state: state, child: const SignUpPage());
+        return buildPageWithDefaultTransition(
+            context: context, state: state, child: const SignUpPage());
       },
     ),
     GoRoute(
       name: 'base',
       path: '/base',
-      builder: (BuildContext context, state) => const BasePage(),
+      pageBuilder: (BuildContext context, state) {
+        return buildPageWithDefaultTransition(
+            context: context, state: state, child: const BasePage());
+      },
+      routes: [
+        GoRoute(
+          name: 'setting',
+          path: 'setting',
+          builder: (BuildContext context, state){
+            return const SettingPage();
+          },
+          routes: [
+            GoRoute(
+              name: 'user_info',
+              path: 'user_info',
+              pageBuilder: (BuildContext context, state){
+                return buildPageWithDefaultTransition(context: context, state: state, child: const UserInfoPage());
+              },
+            ),
+          ],
+        ),
+      ],
     ),
   ],
   initialLocation: initialRoute,
@@ -65,6 +99,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -77,9 +112,14 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp.router(
+        themeMode: ThemeMode.light,
         title: 'Sistem Klinik App',
         theme: ThemeData(
-          fontFamily: 'Manrope',
+            scaffoldBackgroundColor: custWhiteColor,
+            fontFamily: 'Manrope',
+            appBarTheme: const AppBarTheme(
+              systemOverlayStyle: SystemUiOverlayStyle.light,
+            ),
         ),
         routerConfig: _router,
         debugShowCheckedModeBanner: false,
