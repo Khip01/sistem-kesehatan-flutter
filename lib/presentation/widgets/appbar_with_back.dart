68091094133e45a51deccf1d01cb93271a837ac3
Khip01 <aakhifathallah24@gmail.com>
2024-05-21 18:00:59 +0700

    feat: add some widgets

---
+++ lib/presentation/widgets/appbar_with_back.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppbarWithBack extends StatelessWidget {
+  const AppbarWithBack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
