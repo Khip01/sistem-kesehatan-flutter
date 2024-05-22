c07540f9a1555bfe97bdec4ad7be165e5f18e658
Khip01 <aakhifathallah24@gmail.com>
2024-05-22 11:48:45 +0700

    feat: add Appbar With back Button Widget

--- lib/presentation/widgets/appbar_with_back.dart
+++ lib/presentation/widgets/appbar_with_back.dart
@@ -1,10 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:sistem_kesehatan_flutter/presentation/extension/extension.dart';
 
 class AppbarWithBack extends StatelessWidget {
-  const AppbarWithBack({super.key});
+  final Color buttonBackColor;
+  final Color buttonBackIconColor;
+  final String appBarTitleText;
+  final Color appBarTitleColor;
+  final Function() buttonOnTapAction;
+
+  const AppbarWithBack({
+    super.key,
+    required this.buttonBackColor,
+    required this.buttonBackIconColor,
+    required this.appBarTitleText,
+    required this.appBarTitleColor,
+    required this.buttonOnTapAction,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Material(
+            color: Colors.transparent,
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10),
+              onTap: buttonOnTapAction,
+              child: Ink(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: buttonBackColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: buttonBackIconColor,
+                    size: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Text(
+            appBarTitleText,
+            style: TextStyle(
+              fontSize: headingTextSize,
+              color: appBarTitleColor,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            width: 40,
+          ),
+        ],
+      ),
+    );
   }
 }
