aec2d4453c304ef05c258c55410a37dda37d9d63
Khip01 <aakhifathallah24@gmail.com>
2024-05-28 23:07:32 +0700

    feat: add shimmer for loading state
    modify patient screen and doctor screen

--- lib/presentation/pages/base_page_view/dokter_page.dart
+++ lib/presentation/pages/base_page_view/dokter_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sistem_kesehatan_flutter/domain/entities/doctor.dart';
 import 'package:sistem_kesehatan_flutter/presentation/blocs/doctor/doctor_bloc.dart';
 import 'package:sistem_kesehatan_flutter/presentation/extension/extension.dart';
@@ -22,13 +23,13 @@ class DokterPage extends StatelessWidget {
         ),
         Flexible(
           fit: FlexFit.tight,
-          child: _cardListView(),
+          child: _bodyScreen(),
         ),
       ],
     );
   }
 
-  Widget _cardListView() {
+  Widget _bodyScreen() {
     return BlocBuilder<DoctorBloc, DoctorState>(
       builder: (context, state) {
         return state.maybeWhen(
@@ -38,50 +39,144 @@ class DokterPage extends StatelessWidget {
             );
           },
           loading: () {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Center(
+              child: _cardListViewShimmer(),
             );
           },
           success: (doctors) {
-            return ListView.builder(
-              padding: const EdgeInsets.only(top: 20),
-              itemCount: doctors.length,
-              itemBuilder: (BuildContext context, int index) {
-                final doctor = doctors[index];
-                return Container(
-                  height: (MediaQuery.of(context).size.width - 48) / 2.7,
-                  width: double.maxFinite,
-                  margin:
-                      const EdgeInsets.only(left: 24, right: 24, bottom: 24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: custWhiteColor,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 1,
-                        blurRadius: 10,
-                        offset: const Offset(2, 2),
-                        color: custShadowColor,
-                      )
-                    ],
-                  ),
-                  child: LayoutBuilder(
-                    builder: (BuildContext context, BoxConstraints constraint) {
-                      return Row(
-                        children: [
-                          _image(constraint.maxHeight, doctor),
-                          Flexible(
-                            fit: FlexFit.tight,
-                            child: _contentCardInfo(doctor),
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                );
-              },
-            );
+            return _cardListView(doctors);
           },
+        );
+      },
+    );
+  }
+
+  Widget _cardListViewShimmer(){
+      return ListView.builder(
+        padding: const EdgeInsets.only(top: 20),
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: (MediaQuery.of(context).size.width - 48) / 2.7,
+            width: double.maxFinite,
+            margin:
+            const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: custWhiteColor,
+              boxShadow: [
+                BoxShadow(
+                  spreadRadius: 1,
+                  blurRadius: 10,
+                  offset: const Offset(2, 2),
+                  color: custShadowColor,
+                )
+              ],
+            ),
+            child: Shimmer.fromColors(
+              baseColor: custGreyColor,
+              highlightColor: custShadowColor,
+              child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraint) {
+                  return Row(
+                    children: [
+                      Container(
+                        height: constraint.maxHeight - 16,
+                        width: constraint.maxHeight - 16,
+                        margin: const EdgeInsets.all(8),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: BoxDecoration(
+                          color: custPrimaryColor,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      Flexible(
+                        fit: FlexFit.tight,
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            top: 7,
+                            bottom: 7,
+                            left: 8,
+                            right: 16,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+                                width: double.maxFinite,
+                                height: 24,
+                                decoration: BoxDecoration(
+                                  color: custPrimaryColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width / 2,
+                                height: 12,
+                                decoration: BoxDecoration(
+                                  color: custPrimaryColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width / 2,
+                                height: 12,
+                                decoration: BoxDecoration(
+                                  color: custPrimaryColor,
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      );
+    }
+
+  Widget _cardListView(List<Doctor> doctors){
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 20),
+      itemCount: doctors.length,
+      itemBuilder: (BuildContext context, int index) {
+        final doctor = doctors[index];
+        return Container(
+          height: (MediaQuery.of(context).size.width - 48) / 2.7,
+          width: double.maxFinite,
+          margin:
+          const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: custWhiteColor,
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: const Offset(2, 2),
+                color: custShadowColor,
+              )
+            ],
+          ),
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraint) {
+              return Row(
+                children: [
+                  _image(constraint.maxHeight, doctor),
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: _contentCardInfo(doctor),
+                  ),
+                ],
+              );
+            },
+          ),
         );
       },
     );
--- lib/presentation/pages/base_page_view/pasien_page.dart
+++ lib/presentation/pages/base_page_view/pasien_page.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:sistem_kesehatan_flutter/domain/entities/patient.dart';
 import 'package:sistem_kesehatan_flutter/presentation/blocs/patient/patient_bloc.dart';
 
@@ -23,13 +24,13 @@ class PasienPage extends StatelessWidget {
         ),
         Flexible(
           fit: FlexFit.tight,
-          child: _cardListView(),
+          child: _bodyScreen(),
         ),
       ],
     );
   }
 
-  Widget _cardListView() {
+  Widget _bodyScreen() {
     return BlocBuilder<PatientBloc, PatientState>(
       builder: (BuildContext context, state) {
         return state.maybeWhen(
@@ -39,37 +40,104 @@ class PasienPage extends StatelessWidget {
             );
           },
           loading: () {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Center(
+              child: _cardListViewShimmer(),
             );
           },
           success: (patients) {
-            return ListView.builder(
-              padding: const EdgeInsets.only(top: 20),
-              itemCount: patients.length,
-              itemBuilder: (BuildContext context, int index) {
-                final patient = patients[index];
-                return Container(
-                  height: (MediaQuery.of(context).size.width - 48) / 3,
-                  width: double.maxFinite,
-                  margin: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: custWhiteColor,
-                    boxShadow: [
-                      BoxShadow(
-                        spreadRadius: 1,
-                        blurRadius: 10,
-                        offset: const Offset(2, 2),
-                        color: custShadowColor,
-                      )
-                    ],
-                  ),
-                  child: _contentCardInfo(patient),
-                );
-              },
-            );
+            return _cardListView(patients);
           }
+        );
+      },
+    );
+  }
+
+  Widget _cardListViewShimmer(){
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 20),
+      itemCount: 6,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: (MediaQuery.of(context).size.width - 48) / 3,
+          width: double.maxFinite,
+          margin: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: custWhiteColor,
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: const Offset(2, 2),
+                color: custShadowColor,
+              )
+            ],
+          ),
+          child: Shimmer.fromColors(
+            baseColor: custGreyColor,
+            highlightColor: custShadowColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: custPrimaryColor,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 12,
+                    decoration: BoxDecoration(
+                    color: custPrimaryColor,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 12,
+                    decoration: BoxDecoration(
+                      color: custPrimaryColor,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _cardListView(List<Patient> patients){
+    return ListView.builder(
+      padding: const EdgeInsets.only(top: 20),
+      itemCount: patients.length,
+      itemBuilder: (BuildContext context, int index) {
+        final patient = patients[index];
+        return Container(
+          height: (MediaQuery.of(context).size.width - 48) / 3,
+          width: double.maxFinite,
+          margin: const EdgeInsets.only(left: 24, right: 24, bottom: 24),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: custWhiteColor,
+            boxShadow: [
+              BoxShadow(
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: const Offset(2, 2),
+                color: custShadowColor,
+              )
+            ],
+          ),
+          child: _contentCardInfo(patient),
         );
       },
     );
